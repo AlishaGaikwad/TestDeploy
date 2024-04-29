13950e8ebc50d10548a060851ed1db4444042469
AlishaGaikwad <alishagaikwad03@gmail.com>
2024-04-28 18:12:16 -0700

    Initial commit

---
+++ CN-Project-Frontend1/src/modules/Dashboard/Student/StudentAppointmentHistory.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { AppointmentContent } from "./Student.styled";
+import useAPI from "../../../hooks/useAPI";
+
+const StudentAppointmentHistory = () => {
+  const user_id = localStorage.getItem("userId");
+  const [scheduledListData, scheduledList] = useAPI(
+    "GET_STUDENT_SCHEDULE_LIST",
+    {
+      lazy: true,
+    }
+  );
+
+  useEffect(() => {
+    scheduledList({ user_id });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const appointmentData = scheduledListData?.data?.studentItems;
+  
+  return (
+    <div>
+      {appointmentData?.map((appointment) => {
+        return (
+          <AppointmentContent key={appointment.id}>
+            <p>{appointment.reason}</p>
+            <p>{appointment.time}</p>
+          </AppointmentContent>
+        );
+      })}
+    </div>
+  );
+};
+
+export default StudentAppointmentHistory;
